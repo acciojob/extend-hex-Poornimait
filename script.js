4d290ed09019c19e0c70ab09f6f4c38c6db60bea
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-04-02 11:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
+	shortHex = shortHex.replace('#', '');
+	let l1=shortHex[0];
+	let l2=shortHex[1];
+	let l3=shortHex[2];
+	let fullhex="#"+l1+l1+l2+l2+l3+l3;
+	return fullhex;
 };
 
 // Do not change the code below.
